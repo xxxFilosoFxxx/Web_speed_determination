331a87f8b645f485177e114a7c5496b93e8cc958
xxxFilosoFxxx <roman.lashnev@mail.ru>
2021-05-10 22:30:25 +0300

    v1.1.4

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,27 @@
 <template>
-  <h3>{{message}}</h3>
+<!--  <h3>{{message}}</h3>-->
+
+<!--  <label>File-->
+<!--    <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>-->
+<!--  </label>-->
+
+<!--  <div>-->
+<!--    <video width="500" height="300" controls loop>-->
+<!--      <source src="static/data_video/видеонаблюдение.mp4" type='video/mp4'>-->
+<!--      &lt;!&ndash;    <source src="static/data_video/video.webm" type='video/webm'>&ndash;&gt;-->
+<!--      Видео не работает!-->
+<!--    </video>-->
+<!--  </div>-->
+
+  <div>
+    <form action="/" method="post">
+      <label for="video">Select image:</label>
+      <input type="file" id="video" name="video" accept="video/*">
+      <input type="submit">
+    </form>
+  </div>
+
+
 </template>
 
 <script>
